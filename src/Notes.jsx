94495641c8eb5dc779d2f87591a9a7b307187a27
Sunny Kumar <sunny274.kr@gmail.com>
2025-02-19 11:39:11 +0530

    UI updated

--- src/Notes.jsx
+++ src/Notes.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import List from "./Components/List";
 import CreateNew from "./Components/CreateNew";
+import { FaPlus } from "react-icons/fa6";
 
 export default function Notes() {
   const [page, setPage] = useState("list");
@@ -31,27 +32,18 @@ export default function Notes() {
   }, []);
 
   return (
-    <div className="flex flex-col items-center gap-4 py-4 bg-gray-100 min-h-screen">
-      <div className="flex gap-4">
-        <button
-          className="px-4 py-2 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600 transition"
-          onClick={() => {
-            setPage("list");
-          }}
-        >
-          Note List
-        </button>
-        {page === "list" && (
-          <button
-            className="px-4 py-2 bg-green-500 text-white rounded-lg shadow-md hover:bg-green-600 transition"
-            onClick={() => {
-              setPage("create");
-            }}
-          >
-            New Note
-          </button>
-        )}
-      </div>
+    <div className="relative flex flex-col items-center rounded-lg gap-4 py-8 bg-gray-100 min-h-screen">
+      {/* <div className="flex gap-4 relative"> */}
+      <button
+        className="px-4 py-2 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600 transition"
+        onClick={() => {
+          setPage("list");
+        }}
+      >
+        Note List
+      </button>
+
+      {/* </div> */}
       {page === "list" ? (
         <List
           notes={notes}
@@ -65,6 +57,16 @@ export default function Notes() {
           formData={formData}
           setFormData={setFormData}
         />
+      )}
+      {page === "list" && (
+        <button
+          className="fixed right-20 bottom-20 p-4 bg-green-500 text-white rounded-full shadow-md hover:bg-green-600 transition"
+          onClick={() => {
+            setPage("create");
+          }}
+        >
+          <FaPlus className="w-6 h-6" />
+        </button>
       )}
     </div>
   );
